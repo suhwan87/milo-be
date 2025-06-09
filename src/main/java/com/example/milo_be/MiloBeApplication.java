19e2dc9d9b1a4dc9cd82da6fc261abe0f69dc989
suhwan87 <tnghks4875@naver.com>
2025-06-09 16:27:50 +0900

    Test : 환경 세팅

---
+++ src/main/java/com/example/milo_be/MiloBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.milo_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiloBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiloBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/milo_be/MiloBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.milo_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiloBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
