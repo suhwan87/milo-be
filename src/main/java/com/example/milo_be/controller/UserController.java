c8fb52d5790dc5ed366bd6b051ae11f724100ac2
KSY <bc55550@naver.com>
2025-06-26 16:43:15 +0900

    Chore: 사용하지 않는 import문 정리

--- src/main/java/com/example/milo_be/controller/UserController.java
+++ src/main/java/com/example/milo_be/controller/UserController.java
@@ -6,7 +6,6 @@ import com.example.milo_be.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
